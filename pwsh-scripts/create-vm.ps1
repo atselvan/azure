8d1c3eb8920a2792396c9efb646fa09f096ad270
Allan Selvan <allantony2008@gmail.com>
2019-11-03 15:32:01 +0100

    Added powershell infra creation scripts

---
+++ pwsh-scripts/create-vm.ps1
@@ -0,0 +1,43 @@
+# Resource group details
+$rgName = "ps-rg"
+$location = "westeurope"
+
+# Tags
+$tags = @{ Owner = “allantony2008@gmail.com”; Environment = “Labs”; CostCenter = “PSIN001”}
+
+# Virtual network details
+$vnetName = "ps-vnet"
+
+# Virtual Machine details
+$vmName = "vm001"
+$vmSize = "Standard_B1s"
+$pipName = "$vmName-ip"
+$nsgName = "$vmName-nsg"
+$nicName = "$vmName-nic"
+
+# Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Get vnet details
+$vnet = Get-AzVirtualNetwork -Name $vnetName
+
+# Create a public IP address and specify a DNS name
+$pip = New-AzPublicIpAddress -Name $pipName -ResourceGroupName $rgName -Location $location -AllocationMethod Static -IdleTimeoutInMinutes 4 -Tag $tags
+
+# Create an inbound network security group rule for port 3389
+$nsgRuleRDP = New-AzNetworkSecurityRuleConfig -Name "$nsgName-rdp-rule" -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
+
+# Create a network security group
+$nsg = New-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName -Location $location -SecurityRules $nsgRuleRDP
+
+# Create a virtual network card and associate with public IP address and NSG
+$nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+# Create a virtual machine configuration
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize $vmSize | `
+Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $cred | `
+Set-AzVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2019-Datacenter -Version latest | `
+Add-AzVMNetworkInterface -Id $nic.Id
+
+# Create a virtual machine
+New-AzVM -VM $vmConfig -ResourceGroupName $rgName -Location $location
---
+++ pwsh-scripts/create-vnet.ps1
@@ -0,0 +1,32 @@
+
+# Resource group details
+$rgName = "ps-rg"
+$location = "westeurope"
+
+# Tags
+$tags = @{ Owner = “allantony2008@gmail.com”; Environment = “Labs”; CostCenter = “PSIN001”}
+
+# Virtual network details
+$vnetName = "ps-vnet"
+$vnetAddressPrefix = "10.0.0.0/22"
+
+# Subnet details
+$subnet01Name = "subnet01"
+$subnet01AddressPrefix = "10.0.0.0/24"
+$subnet02Name = "subnet02"
+$subnet02AddressPrefix = "10.0.1.0/24"
+$subnet03Name = "subnet03"
+$subnet03AddressPrefix = "10.0.2.0/24"
+
+# Create subnet config
+$subnet01 = New-AzVirtualNetworkSubnetConfig -Name $subnet01Name -AddressPrefix $subnet01AddressPrefix
+$subnet02 = New-AzVirtualNetworkSubnetConfig -Name $subnet02Name -AddressPrefix $subnet02AddressPrefix
+
+# Create vnet with two subnets
+$vnet = New-AzVirtualNetwork -Name $vnetName -ResourceGroupName $rgName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet01, $subnet02 -Tag $tags
+
+# Modifying the vnet created to add the third subnet
+Add-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnet03Name -AddressPrefix $subnet03AddressPrefix
+
+# Save the additional subnet config
+Set-AzVirtualNetwork -VirtualNetwork $vnet
